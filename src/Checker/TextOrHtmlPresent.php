0349ab14479eeff40448587d41d97a86db11d19b
Nicolas Hohm <nickel7152@gmail.com>
2016-02-13 23:18:09 +0100

    Add text or html present checker

---
+++ src/Checker/TextOrHtmlPresent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WebsiteMonitoring\Checker;
+
+use WebsiteMonitoring\WebsiteConfig;
+
+class TextOrHtmlPresent implements CheckerInterface
+{
+    public function check(WebsiteConfig $config, array $checkerConfig = [])
+    {
+        // TODO: Implement check() method.
+    }
+
+    public function parse(WebsiteConfig $config, array $checkerConfig = [])
+    {
+        // TODO: Implement parse() method.
+    }
+}
